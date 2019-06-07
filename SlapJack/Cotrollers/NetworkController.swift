3c84b23d56721a90a0d7d0a8fa459429efe31d5f
benjamin7p <benjamin7p@gmail.com>
2019-06-07 09:27:46 -0600

    cleaning

--- SlapJack/Cotrollers/NetworkController.swift
+++ SlapJack/Cotrollers/NetworkController.swift
@@ -14,13 +14,11 @@ class NetworkController {
     static func performNetworkRequest(for url: URL, completion: ((Data?, Error?)-> Void)? = nil){
         
         let request = URLRequest(url: url)
-        
         let dataTask = URLSession.shared.dataTask(with: request) {(data, response, error) in
             if let completion = completion {
                 completion(data, error)
             }
         }
-        
         dataTask.resume()
     }
 }
